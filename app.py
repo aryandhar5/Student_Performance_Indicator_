4d9a13e785a59edf592f970f47f3441fa980fb72
aryandhar18 <aryandhar18@gmail.com>
2024-06-03 00:57:10 +0530

    model trainer

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sys
 from src.Student_performance_indicator.components.data_ingestion import DataIngestion
 from src.Student_performance_indicator.components.data_ingestion import DataIngestionConfig
 from src.Student_performance_indicator.components.data_transformation import DataTransformationConfig,DataTransformation
-
+from src.Student_performance_indicator.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 if __name__=="__main__":
     logging.info("the execution has started")
@@ -17,7 +17,12 @@ if __name__=="__main__":
 
         #data_transformation_config=DataTransformationConfig()
         data_transformation=DataTransformation()
-        data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+        train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data_path,test_data_path)
+
+
+        model_trainer=ModelTrainer()
+        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
+       
         
     except Exception as e:
         logging.info("Custom exception")
